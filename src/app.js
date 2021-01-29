fe8cda7a293bf12185face2884e4831afac4e799
Lucas-Rodrigues1985 <72477507+Lucas-Rodrigues1985@users.noreply.github.com>
2021-01-28 22:16:39 -0300

    final

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 window.onload = () => {
-  // Seu código aqui
+const BotaoMenos = document.querySelector('.quantidade-menos')
+const BotaoValor = document.querySelector('.quantidade-valor')
+const BotaoMais = document.querySelector('.quantidade-mais')
+const Carrinho = document.querySelector('.compra')
+
+let quantidade = 1
+
+BotaoMenos.addEventListener('click', () => {
+ quantidade = quantidade - 1
+ BotaoValor.innerHTML = quantidade
+ Carrinho.innerHTML = 'Carrinho (' + quantidade + ')'
+})
+
+BotaoMais.addEventListener('click', () => {
+ quantidade = quantidade + 1
+ BotaoValor.innerHTML = quantidade
+ Carrinho.innerHTML = 'Carrinho (' + quantidade + ')'
+})
 }
